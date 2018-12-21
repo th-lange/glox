74802f1f70306e4b756f2719d4bea92f94384373
Thomas Lange <thomas.lange@smartmobilefactory.com>
2018-12-21 13:29:44 +0100

    [3-GenerateAST]: Refactored AST generation
    - Refactored AST generation
    - Added Expression struct and corresponding interfaces

--- main.go
+++ main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-	"github.com/th-lange/glox/base"
-	"github.com/th-lange/glox/cmd"
 	"path"
 	"runtime"
+
+	"github.com/th-lange/glox/base"
+	"github.com/th-lange/glox/cmd"
 )
 
 var HomeDir string
 
-
 func main() {
 
 	_, executionPath, _, ok := runtime.Caller(0)
-	if !ok{
+	if !ok {
 		panic("No information about execution")
 	}
 	base.HomeDir = path.Dir(executionPath)
 	cmd.Execute()
 }
-
