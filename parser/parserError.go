3abedcd8e39c612b5d65f682314d5b70f5bf25f7
Thomas Lange <thomas.lange@smartmobilefactory.com>
2018-12-26 10:40:01 +0100

    Adding Parser Error Handling and Sync

--- parser/parserError.go
+++ parser/parserError.go
@@ -6,12 +6,37 @@ import (
 	"github.com/th-lange/glox/scanner"
 )
 
-type ParserError struct {
+// Indicates that the PARSED code is erroneous
+type ParsingError struct {
 	ErrorStart    scanner.Token
 	TokenPosition int
 	Message       string
+	SyncEnd       *scanner.Token
 }
 
-func (se ParserError) Error() string {
-	return "[Token Position " + strconv.Itoa(se.TokenPosition) + "] Last Valid Element" + (se.ErrorStart).String() + "! " + se.Message
+func (pe ParsingError) Error() string {
+	endMsg := ""
+	if pe.SyncEnd != nil {
+		endMsg = "\nSynced Up to Element: " + pe.SyncEnd.String()
+	}
+	return "[Token Position " + strconv.Itoa(pe.TokenPosition) + "] Last Valid Element" + (pe.ErrorStart).String() + "!\n" + pe.Message + endMsg
+}
+
+func NewError(message string, sync bool, prs *parser) ParsingError {
+
+	err := ParsingError{prs.current(), prs.head, message, nil}
+	if sync {
+		prs.synchronize()
+		err.SyncEnd = &(*prs.tokens)[prs.head]
+	}
+	return err
+}
+
+// Indicates issues with the PARSER code is erroneous
+type InvalidArgumentError struct {
+	message string
+}
+
+func (iae InvalidArgumentError) Error() string {
+	return iae.message
 }
